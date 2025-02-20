ae2e9ab85fcb59221304fb776573aa47436268de
MumflrFumperdink <47613982+MumflrFumperdink@users.noreply.github.com>
2025-02-20 10:51:04 -0600

    ODEs: Added Precalculated Flows and Automatic Flow Arrays

--- ode/js/flow2D.js
+++ ode/js/flow2D.js
@@ -1,27 +1,53 @@
-class Flow2D {
-	constructor(initialX, initialY, color) {
-		this.position = {x: initialX, y: initialY};
+const numIterations = 10000;
 
+class Flow2D {
+	constructor(initialX, initialY, firstOrderDiffEqX, firstOrderDiffEqY, dt, color, preCalculateFlows = false) {
 		this.initialX = initialX;
 		this.initialY = initialY;
 
 		this.flowX = [initialX];
 		this.flowY = [initialY];
 
+		this.firstOrderDiffEqX = firstOrderDiffEqX;
+		this.firstOrderDiffEqY = firstOrderDiffEqY;
+
+		this.dt = dt;
 		this.color = color;
+		this.preCalculateFlows = preCalculateFlows;
+
+		if (this.preCalculateFlows) {
+			for (let i = 0; i < numIterations; i++) {
+				this.calculateNextFlowState();
+			}
+		}
+
+		this.currentPositionIndex = 0;
 	}
 
-	update(dt, firstOrderDiffEqX, firstOrderDiffEqY) {
-		this.position.x += firstOrderDiffEqX(this.position.x, this.position.y) * dt;
-		this.position.y += firstOrderDiffEqY(this.position.x, this.position.y) * dt;
+	calculateNextFlowState() {
+		const i = this.flowX.length-1;
 
-		this.flowX.push(this.position.x);
-		this.flowY.push(this.position.y);
+		const nextX = this.flowX[i] + this.firstOrderDiffEqX(this.flowX[i], this.flowY[i]) * this.dt;
+		const nextY = this.flowY[i] + this.firstOrderDiffEqY(this.flowX[i], this.flowY[i]) * this.dt;
+
+		this.flowX.push(nextX);
+		this.flowY.push(nextY);
+	}
+
+	update() {
+		if (!this.preCalculateFlows) this.calculateNextFlowState();
+
+		if (this.currentPositionIndex < numIterations - 1) {
+			this.currentPositionIndex++;
+		}
+		else {
+			console.log("!sx");
+		}
 	}
 
 	draw() {
 		drawPlotCoordCurve(this.flowX, this.flowY, this.color);
-		drawCircle(this.initialX, this.initialY, 2, "Black");
-		drawCircle(this.position.x, this.position.y, 4, this.color);
+		drawCircle(this.initialX, this.initialY, 2, "Red");
+		drawCircle(this.flowX[this.currentPositionIndex], this.flowY[this.currentPositionIndex], 4, this.color);
 	}
 }
--- ode/js/script.js
+++ ode/js/script.js
@@ -1,6 +1,9 @@
 let plot, div;
 
-let currentFlow;
+const currentFlows = [];
+let precalcFlows = false;
+
+const dt = 0.010;
 
 let currentDiffEqX = (x, y) => -y*y;
 let currentDiffEqY = (x, y) => x-0.3*y;
@@ -53,8 +56,8 @@ const mainScreen = async () => {
   const drawing_canvas = document.createElement("canvas");
   drawing_canvas.width = plot_boundbox.width;
   drawing_canvas.height = plot_boundbox.height;
-  drawing_canvas.style.left = plot_boundbox.x + "px";
-  drawing_canvas.style.top = plot_boundbox.y + "px";
+  drawing_canvas.style.left = (plot_boundbox.x + 7) + "px";
+  drawing_canvas.style.top = (plot_boundbox.y - 7) + "px";
 
   pixelBBox.min_x = 0;
   pixelBBox.min_y = 0;
@@ -67,30 +70,32 @@ const mainScreen = async () => {
   ctx.lineWidth = 1;
 
   drawing_canvas.addEventListener("click", async (e) => {
-    currentFlow = new Flow2D(p5map(e.layerX, pixelBBox.min_x, pixelBBox.max_x, plotBBox.min_x, plotBBox.max_x),
+    const currentFlow = new Flow2D(p5map(e.layerX, pixelBBox.min_x, pixelBBox.max_x, plotBBox.min_x, plotBBox.max_x),
                              p5map(e.layerY, pixelBBox.max_y, pixelBBox.min_y, plotBBox.min_y, plotBBox.max_y),
-                             "red");
-
-    for (let i = 0; i < 10000; i++) {
-      await new Promise(resolve => setTimeout(resolve, 10));
-      updateGraph();
-    }
+                             currentDiffEqX,
+                             currentDiffEqY,
+                             dt,
+                             "black",
+                             precalcFlows);
+    currentFlows.push(currentFlow);
   });
+
+  this.interval = setInterval(updateGraph, dt*1000);
 };
 
-let updateGraph = () => {
+const updateGraph = () => {
   ctx.clearRect(0, 0, pixelBBox.max_x, pixelBBox.max_y);
 
-  if (currentFlow) {
-    currentFlow.update(0.010, currentDiffEqX, currentDiffEqY);
+  for (currentFlow of currentFlows) {
+    currentFlow.update();
     currentFlow.draw();
   }
 };
 
 const xDerivativeInputField = document.getElementById("x_deriv_input");
 const yDerivativeInputField = document.getElementById("y_deriv_input");
-let updateCanvas = () => {
-  currentFlow = null;
+const resetVectorField = () => {
+  currentFlows.length = 0;
   ctx.clearRect(0, 0, pixelBBox.max_x, pixelBBox.max_y);
 
   currentDiffEqX = Function("x", "y", "return " + xDerivativeInputField.value);
@@ -106,6 +111,68 @@ let updateCanvas = () => {
   plot = createPlot(vectorField);
   div.removeChild(div.firstChild);
   div.append(plot);
+};
+
+const createFlowArray = () => {
+  const intervalBetweenFlows = 0.25;
+
+  const potentialStartPointsX = [];
+  const potentialStartPointsY = [];
+  const potentialStartPointsPointingIntoScreen = [];
+
+  // top
+  for (let i = 0; i < (plotBBox.max_x - plotBBox.min_x) / intervalBetweenFlows; i++) {
+    potentialStartPointsX.push(plotBBox.min_x + i * intervalBetweenFlows);
+    potentialStartPointsY.push(plotBBox.max_y);
+
+    const isPointingDown = currentDiffEqY(potentialStartPointsX.at(-1), potentialStartPointsY.at(-1)) < 0;
+    potentialStartPointsPointingIntoScreen.push(isPointingDown);
+  }
+
+  // bottom
+  for (let i = 0; i < (plotBBox.max_x - plotBBox.min_x)  / intervalBetweenFlows; i++) {
+    potentialStartPointsX.push(plotBBox.min_x + i * intervalBetweenFlows);
+    potentialStartPointsY.push(plotBBox.min_y);
+
+    const isPointingUp = currentDiffEqY(potentialStartPointsX.at(-1), potentialStartPointsY.at(-1)) > 0;
+    potentialStartPointsPointingIntoScreen.push(isPointingUp);
+  }
+
+  // left
+  for (let i = 0; i < (plotBBox.max_y - plotBBox.min_y) / intervalBetweenFlows; i++) {
+    potentialStartPointsX.push(plotBBox.min_x);
+    potentialStartPointsY.push(plotBBox.min_y + i * intervalBetweenFlows);
+
+    const isPointingRight = currentDiffEqX(potentialStartPointsX.at(-1), potentialStartPointsY.at(-1)) > 0;
+    potentialStartPointsPointingIntoScreen.push(isPointingRight);
+  }
+
+  // right
+  for (let i = 0; i < (plotBBox.max_y - plotBBox.min_y)  / intervalBetweenFlows; i++) {
+    potentialStartPointsX.push(plotBBox.max_x);
+    potentialStartPointsY.push(plotBBox.min_y + i * intervalBetweenFlows);
+
+    const isPointingLeft = currentDiffEqX(potentialStartPointsX.at(-1), potentialStartPointsY.at(-1)) < 0;
+    potentialStartPointsPointingIntoScreen.push(isPointingLeft);
+  }
+
+  console.log(potentialStartPointsX);
+  console.log(potentialStartPointsY);
+  console.log(potentialStartPointsPointingIntoScreen);
+
+  // Add in apropriate flows
+  for (let i = 0; i < potentialStartPointsX.length; i++) {
+    if (potentialStartPointsPointingIntoScreen[i]) {
+      const currentFlow = new Flow2D(potentialStartPointsX[i],
+                             potentialStartPointsY[i],
+                             currentDiffEqX,
+                             currentDiffEqY,
+                             dt,
+                             "black",
+                             precalcFlows);
+      currentFlows.push(currentFlow);
+    }
+  }
 };
 
 
